fcbe182cc75e2971391c2b56577ea6f7f6227356
sarahwatremet <sarah.watremet@gmail.com>
2022-10-04 15:38:11 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sarah WATREMET on 04/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
